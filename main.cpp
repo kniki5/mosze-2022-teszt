88fd2d151c02d3fe101ff93b693523d2ae434e62
kniki5 <100291147+kniki5@users.noreply.github.com>
2022-09-10 21:59:22 +0200

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -4,22 +4,19 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];                            //javitas: int *b = new int[N_ELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'                  //javitas: std::cout << "1-100 ertekek duplazasa" << std::endl;
-    for (int i = 0;)                                        //javitas: for (int i = 0; i < N_ELEMENTS; i++)
+    int *b = new int[N_ELEMENTS];                           //javitas: int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;    //javitas: std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)                    //javitas: for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2;                                       //javitas: std::cout << "Ertek: " << b[i] << std::endl;
-    }                                                       //javitas: };
-    for (int i = 0; i; i++)                                 //javitas: nincs szukseg erre a for ciklusra
-    {
-        std::cout << "Ertek:"     
-    }    
+        b[i] = i * 2;                 
+        std::cout << "Ertek: " << b[i] << std::endl;        //javitas: std::cout << "Ertek: " << b[i] << std::endl;
+    };                                                      //javitas: };
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;                                              //javitas: int atlag = 0;
+    int atlag = 0                                           //javitas: int atlag = 0;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]                                       //javitas: atlag += b[i];
-    }                                                       //javitas: };
+        atlag += b[i];                                      //javitas: atlag += b[i];
+    };                                                      //javitas: };
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
