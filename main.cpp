03657639c8e9dcdede5a287782ac8303035966a0
kniki5 <100291147+kniki5@users.noreply.github.com>
2022-09-10 22:00:17 +0200

    Updated main.cpp 2.0

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main()
         std::cout << "Ertek: " << b[i] << std::endl;        //javitas: std::cout << "Ertek: " << b[i] << std::endl;
     };                                                      //javitas: };
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag = 0                                           //javitas: int atlag = 0;
+    int atlag = 0;                                          //javitas: int atlag = 0;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i];                                      //javitas: atlag += b[i];
